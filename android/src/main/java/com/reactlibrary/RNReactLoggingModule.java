e02af3f16f8080624a28f240878c4510aa81ad0f
emad-salah <emads14@gmail.com>
2020-01-01 17:41:34 +0100

    Change package name to avoid conflicts

--- android/src/main/java/com/reactlibrary/RNReactLoggingModule.java
+++ android/src/main/java/com/reactlibrary/RNReactLoggingModule.java
@@ -1,5 +1,5 @@
 
-package com.reactlibrary;
+package com.reactfilelogger;
 
 import android.util.Log;
 import android.os.Environment;
--- android/src/main/java/com/reactlibrary/RNReactLoggingPackage.java
+++ android/src/main/java/com/reactlibrary/RNReactLoggingPackage.java
@@ -1,5 +1,5 @@
 
-package com.reactlibrary;
+package com.reactfilelogger;
 
 import java.util.Arrays;
 import java.util.Collections;
