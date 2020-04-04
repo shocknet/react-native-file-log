0cc048d508d6146652b14bdc31d2e3085559152b
Emad-salah <emads14@gmail.com>
2020-04-04 22:56:43 +0100

    Bug fixes

--- android/src/main/java/com/reactlibrary/RNReactLoggingModule.java
+++ android/src/main/java/com/reactlibrary/RNReactLoggingModule.java
@@ -29,7 +29,7 @@ public class RNReactLoggingModule extends ReactContextBaseJavaModule {
     private String tag = "RNReactLogging";
     private boolean consoleLog = true;
     private boolean fileLog = false;
-    private long maxFileSize = 512 * 1024; // 512 kb
+    private int maxFileSize = 512 * 1024; // 512 kb
 
     public RNReactLoggingModule(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -161,7 +161,7 @@ public class RNReactLoggingModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void setMaxFileSize(long maxFileSize) {
+    public void setMaxFileSize(int maxFileSize) {
         this.maxFileSize = maxFileSize;
     }
 
